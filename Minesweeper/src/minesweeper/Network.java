ad0b3eda99b5e91e847a9280087670420a998ba5
Bányai Tamás <BanTam35@users.noreply.github.com>
2015-04-12 19:38:13 +0200

    Nerwork: IPV4 regexp check function added

--- Minesweeper/src/minesweeper/Network.java
+++ Minesweeper/src/minesweeper/Network.java
@@ -1,5 +1,8 @@
 package minesweeper;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 abstract class Network {
 	protected Control ctrl;
 
@@ -12,5 +15,14 @@ abstract class Network {
 	abstract void disconnect();
 
 	abstract void sendBoard(Board messageBoard);
+	
+	private static final String IPV4PATTERN = "^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+	
+	public static boolean validate(final String ipv4String){          
+
+	      Pattern pattern = Pattern.compile(IPV4PATTERN);
+	      Matcher matcher = pattern.matcher(ipv4String);
+	      return matcher.matches();
+	}
 
 }
