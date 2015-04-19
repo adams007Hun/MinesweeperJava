d5db1e3db906dc3a285aa6182c3bae752b586de7
Bányai Tamás <BanTam35@users.noreply.github.com>
2015-04-19 13:18:36 +0200

    Network Debug code removed - Network functional (except IP handling)

--- Minesweeper/src/minesweeper/Main.java
+++ Minesweeper/src/minesweeper/Main.java
@@ -11,26 +11,6 @@ public class Main
 		Control c = new Control();
 		MinesweeperGui gui = new MinesweeperGui(c);
 		c.setGUI(gui);
-		
-		
-		class ClientProbe implements Runnable {
-
-			public void run() {
-				Control cClient = new Control();
-				cClient.startClient();
-			}
-		}
-		
-		c.startServer();
-		Thread rec = new Thread(new ClientProbe());
-		rec.start();
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		c.sendBoard(c.getLocalBoard());
 	}
 
 }
